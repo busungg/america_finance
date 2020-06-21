b15dbd3145209b6048ce18d841fbcc19455891ed
busungg <busungg@naver.com>
2020-06-21 15:00:36 +0900

    practice http request and response to file
      1. http request
      2. http response to file(excel)

---
+++ routes/finance.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios').default;
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    axios.get('https://stockrow.com/api/companies/AMZN/financials.xlsx?dimension=Q&section=Metrics&sort=desc',
+    {
+        responseType: 'blob',
+        headers: {
+            'content-type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        }
+    })
+    .then(function (response) {
+        fs.writeFileSync(
+            path.join(__dirname,
+            '../public/data', 
+            'output.xlsx'), 
+            response.data, 
+            'binary', 
+            function (err) {
+                console.log(err);
+            });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+    
+    res.send('respond with a resource');
+});
+
+module.exports = router;
